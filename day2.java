670eb1f075a9104d7e77e209cd5e7a0706489576
Kavin <kavin88701@gmail.com>
2025-07-08 11:23:10 +0530

    java

---
+++ day2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class day2{
+    public static void main(String[] args)
+    {
+        Scanner kavin = new Scanner(System.in);
+        System.out.print("Enter a name :");
+        String name = kavin.nextLine();
+        System.out.print("Enter age :");
+        int age = kavin.nextInt();
+        kavin.nextLine();
+        System.out.print("Enter address :");
+        String add = kavin.nextLine();
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+        System.out.print("My Address is "+add);
+       
+        // Scanner kavin=new Scanner(System.in);
+        // int a=kavin.nextInt();
+        // int b=kavin.nextInt();
+        // int c=kavin.nextInt();
+        // int d=a*b*c;
+        // int e=a+b+c;
+        // System.out.println("The multiplied value : "+d);
+        // System.out.print("The added value "+e);
+        // System.out.print("\nThe Value divide (d/e)");
+        // System.out.print(d/e);
+
+
+        // Scanner kavin=new Scanner(System.in);
+        // System.out.print("Enter Your Name: ");
+        // String main=kavin.nextLine();
+
+        // System.out.print("\nEnter the Score :");
+        // double score=kavin.nextDouble();
+        // kavin.nextLine();
+
+        // System.out.print("\nEnter the Dept name :");
+        // String dept=kavin.nextLine();
+
+       // double Cal=(score/100.0)*10;
+
+        //System.out.print("\nThe Converted Score is :");
+        //System.out.printf("\nThe Score out of 10 :%.2f\n",Cal);
+        //System.out.print("The Score out of 10 is :"+score/10);
+
+
+    }
+}
