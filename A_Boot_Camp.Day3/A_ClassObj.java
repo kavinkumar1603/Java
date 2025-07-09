c7e79131413e03846daf81c8b1f2c5d1dc86ca0f
Kavin <kavin88701@gmail.com>
2025-07-09 10:10:23 +0530

    file

--- A_Boot_Camp.Day3/A_ClassObj.java
+++ A_Boot_Camp.Day3/A_ClassObj.java
@@ -33,6 +33,7 @@ public class A_ClassObj
 		// r.addition(a, b);
 		// System.out.println("Addition of two numbers is : " + r.c);
 		System.out.println("Addition of two numbers is : " + r.addition(a, b));
+		sc.close();
 	} 
 }
 
